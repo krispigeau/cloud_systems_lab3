9790f897d0f21dad0bd8a0f3e9ea091757b9060e
Kris Pigeau <kris.pigeau@gmail.com>
2023-04-01 09:25:58 -0400

    security group working refactored vpc varaibles and module name

--- main.tf
+++ main.tf
@@ -1,36 +1,34 @@
-module "vpc" {
+module "vpc_1" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
-
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-
-  azs             = var.vpc_azs
-  private_subnets = var.vpc_private_subnets
-  public_subnets  = var.vpc_public_subnets
-
-  enable_nat_gateway = var.vpc_enable_nat_gateway
-
+  name = var.vpc_a_name
+  cidr = var.vpc_a_cidr
+  azs             = var.vpc_a_azs
+  private_subnets = var.vpc_a_private_subnets
+  public_subnets  = var.vpc_a_public_subnets
+  enable_nat_gateway = var.vpc_a_enable_nat_gateway
 }
 
 module "lab3_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
 
-  name        = "user-service"
+  name        = "lab3-sg"
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.vpc_1.vpc_id
 
-  ingress_cidr_blocks = ["10.10.0.0/16"]
-  ingress_rules       = ["http-80-tcp","ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
 }
 
 module "ec2_public" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "4.3.0"
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "4.3.0"
   ami                    = "ami-00c39f71452c08778"
   instance_type          = "t2.micro"
-  name  = "${var.vpc_name}-public"
+  name                   = "${var.vpc_a_name}-public"
   vpc_security_group_ids = [module.lab3_sg.security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+  subnet_id              = module.vpc_1.public_subnets[0]
 }
