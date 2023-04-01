9f521eaee2d66f164c1a7496be5d14980750ae64
Kris Pigeau <kris.pigeau@gmail.com>
2023-03-31 20:27:40 -0400

    done for today

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.49.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  version = "3.19.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
@@ -24,23 +11,26 @@ module "vpc" {
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
 
-  tags = var.vpc_tags
 }
 
-module "ec2_instances" {
+module "lab3_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+  ingress_rules       = ["http-80-tcp","ssh-tcp"]
+}
+
+module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.3.0"
-
-  count = 2
-  name  = "my-ec2-cluster-${count.index}"
-
-  ami                    = "ami-007855ac798b5175e"
+  ami                    = "ami-00c39f71452c08778"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  name  = "${var.vpc_name}-public"
+  vpc_security_group_ids = [module.lab3_sg.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
 }
